9122de399a1b2348a23e1089ce92f0f5953eaf37
Enrico Ortenzi <b00156016@mytudublin.ie>
2023-11-10 18:46:03 +0000

    WebGL

--- Football Scripting Challenge 4/Assets/Challenge 4/Scripts/OpenLink.cs
+++ Football Scripting Challenge 4/Assets/Challenge 4/Scripts/OpenLink.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class OpenLink : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public void OpenChannel() {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Application.OpenURL("https://github.com/Tommy-Vercetty/football-challenge4");
     }
 }
